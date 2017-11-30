9f8a4632625980b4c7e5b7a9b735011caca9efba
edgar <spickerman.edgar@gmail.com>
2017-11-30 14:18:20 -0800

    added details view and route lists view and route forms view and route

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', (req, res) => res.render('index', { title: 'Express' }));
+
+router.get('/lists/:models/:filter', (req, res) => res.render('lists', { title: 'Express' }));
+
+router.get('/details/:models/:id', (req, res) => res.render('details', { title: 'Express' }));
+
+router.get('/new/:models', (req, res) => res.render('forms', { title: 'Express' }));
 
 module.exports = router;
